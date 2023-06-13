57742dc3456b9652d09c820ecdb117066fc8062a
switchaime <switchaime4@gmail.com>
2023-06-13 08:58:31 +0100

    Retrait des chaines de caractères dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:le_grand_magazine/frontend/pages/home_page.dart';
+import 'package:le_grand_magazine/frontend/utils/app_strings.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark));
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Le Grand Magazine',
+      title: AppStrings.appName,
       theme: ThemeData(primarySwatch: Colors.red, visualDensity: VisualDensity.adaptivePlatformDensity),
       debugShowCheckedModeBanner: false,
       home:  const HomePage(),
