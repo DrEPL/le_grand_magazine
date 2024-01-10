9f5a03e82b3d3afe87f25e7e40b473e9b5ab481c
DrEPL <dolnickenzanza@gmail.com>
2024-01-10 17:56:21 +0100

    fix: all

--- lib/frontend/utils/api_link.dart
+++ lib/frontend/utils/api_link.dart
@@ -1,6 +1,6 @@
 const String ipAdress = "https://drepl.pythonanywhere.com/api";
 // const String ipAdress = "http://192.168.100.205:8000/api";
-const String articlesLink = "$ipAdress/";
+const String articlesLink = "$ipAdress/articles/";
 const String categoriesLink = "$ipAdress/categories/";
 const String editionsLink = "$ipAdress/editions/";
 const String videosLink = "$ipAdress/videos/";
